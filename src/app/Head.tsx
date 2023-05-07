e56660cf16e55f44a55c27f2a35f7d20f81b4906
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-05-07 18:53:40 -0300

    chore: dump next version to 12.3

--- src/app/Head.tsx
+++
@@ -1,8 +0,0 @@
-export const Head = () => {
-    return (
-        <>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        </>
-    );
-};
