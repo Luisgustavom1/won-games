9fe8376845d41d7809c8614a2a295491f861a6cd
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-05-07 17:58:12 -0300

    feat: metadata

---
+++ src/app/Head.tsx
@@ -0,0 +1,8 @@
+export const Head = () => {
+    return (
+        <>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        </>
+    );
+};
