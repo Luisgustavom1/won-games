2fa1b7686d1db14a34bbb0da868baac362e64ddb
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-08-27 10:15:42 -0300

    fix: storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,20 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-import path from 'path';
 
 const config: StorybookConfig = {
-    stories: ['../src/components/**/stories.tsx'],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+    stories: ['../src/components/**/*.stories.tsx'],
+    staticDirs: ['../public'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-onboarding',
+        '@storybook/addon-interactions',
+    ],
     framework: {
         name: '@storybook/nextjs',
         options: {},
     },
     docs: {
         autodocs: 'tag',
-    },
-    webpackFinal: (config) => {
-        if (!config.resolve) return config;
-
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@/': path.resolve(__dirname, '../src/'),
-        };
-
-        return config;
     },
 };
 export default config;
